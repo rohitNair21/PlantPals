f241b49abdf57ed5c5b4a255875b802378fdf58c
Rohit Nair <nairrohit003@gmail.com>
2024-02-17 17:29:40 -0700

    Modified further

--- pages/Login.js
+++ pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
 import { Input, Button } from "react-native-elements";
-import Logo from "/Users/rohitnair/PlantPals/assets/Logo_1.svg";
+import Logo from "../assets/Logo_1.svg";
 import SvgUri from "react-native-svg-uri";
 
 const Login = () => {
@@ -10,33 +10,51 @@ const Login = () => {
 
   return (
     <View style={styles.container}>
-      <Image source={Logo} />
-      <Text style={styles.title}>PlantPals 🌱</Text>
-      <Input
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-        autoCapitalirze="none"
-        containerStyle={styles.input}
+      <Image source={Logo} style={styles.logo} resizeMode="contain" />
+      <SvgUri
+        width="200"
+        height="200"
+        source={{
+          uri: "../assets/Logo_1.svg",
+        }}
       />
-      <Input
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        containerStyle={styles.input}
-      />
-      <TouchableOpacity onPress={() => console.log("Forgot Password pressed")}>
-        <Text style={styles.forgotPassword}>Forgot password?</Text>
-      </TouchableOpacity>
-      <Button
-        title="Log in"
-        buttonStyle={styles.loginButton}
-        onPress={() => console.log("Login pressed")}
-      />
-      <TouchableOpacity onPress={() => console.log("Sign Up pressed")}>
-        <Text style={styles.signUpText}>Don’t have an account? Sign up</Text>
-      </TouchableOpacity>
+      <View style={styles.loginBox}>
+        <Text style={styles.title}>Login</Text>
+        <Text style={styles.header}>Username</Text>
+        <Input
+          style={styles.input}
+          value={username}
+          onChangeText={setUsername}
+          autoCapitalize="none"
+          inputContainerStyle={styles.inputContainer}
+          inputStyle={styles.input}
+          containerStyle={styles.inputContainerStyle}
+        />
+
+        <Text style={styles.header}>Password</Text>
+        <Input
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+          inputContainerStyle={styles.inputContainer}
+          inputStyle={styles.input}
+          containerStyle={styles.inputContainerStyle}
+        />
+        <TouchableOpacity
+          onPress={() => console.log("Forgot Password pressed")}
+        >
+          <Text style={styles.forgotPassword}>Forgot password?</Text>
+        </TouchableOpacity>
+        <Button
+          title="Log in"
+          buttonStyle={styles.loginButton}
+          onPress={() => console.log("Login pressed")}
+        />
+        <TouchableOpacity onPress={() => console.log("Sign Up pressed")}>
+          <Text style={styles.signUpText}>Don’t have an account? Sign up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -44,35 +62,66 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center", // Center the box vertically
     alignItems: "center",
     padding: 20,
     backgroundColor: "#F9FAF9",
   },
   logo: {
-    width: 150,
-    height: 150,
-    marginTop: 60,
+    width: 120,
+    height: 120,
     marginBottom: 32,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 48,
-  },
   input: {
-    width: "100%",
-    marginBottom: 16,
+    opacity: 0.8,
+    borderColor: "#000",
+    fontSize: 15,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  loginBox: {
+    width: "80%",
+    alignItems: "center",
+    backgroundColor: "#E1F0C8",
+    borderRadius: 20,
+    paddingVertical: 40,
+    paddingHorizontal: 20,
+    elevation: 10,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginBottom: 24,
+    color: "#333",
+    alignSelf: "start",
+  },
+  inputContainer: {
+    borderBottomWidth: 0, // Remove bottom border
+    backgroundColor: "#eee", // Light grey background
+    borderRadius: 10, // Rounded corners
+    marginBottom: 16, // Margin between input fields
+  },
+  inputContainerStyle: {
+    paddingHorizontal: 0, // No additional horizontal padding
   },
   forgotPassword: {
-    alignSelf: "flex-end",
-    marginVertical: 12,
+    marginBottom: 12,
     color: "#6B7280",
+    alignSelf: "end",
+  },
+  header: {
+    alignSelf: "start",
   },
   loginButton: {
-    backgroundColor: "#19B381",
+    backgroundColor: "#1E8523", // A green shade for the login button
     borderRadius: 25,
-    paddingVertical: 10,
-    paddingHorizontal: 25,
+    paddingVertical: 12,
+    width: "100%", // Full width button
+    marginTop: 10, // Space above the button
   },
   signUpText: {
     marginTop: 24,
