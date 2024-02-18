7c2eacec2395aca25d7c274e4c33ef9d6c31e080
Makiv1 <mvujic@hotmail.com>
2024-02-17 20:13:32 -0700

    MapView Added

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import {PROVIDER_GOOGLE} from 'react-native-maps';
+
+const calgary = {
+  latitude: 51.0478,
+  longitude: 114.0593
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView initialRegion={calgary}  style={styles.map} provider={PROVIDER_GOOGLE}/>
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +24,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
